943de0bee1150d80a59b426186f560cc93774a54
Chris Fordham <chris@fordham.id.au>
2020-09-17 12:50:34 +1000

    Revert.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+module "files" {
+  source  = "matti/resource/shell"
+  command = "ls -l"
+}
+
+output "my_files" {
+  value = module.files.stdout
+}
+
 # TODO: complete
 
 # output "k3s_kube_config" {
